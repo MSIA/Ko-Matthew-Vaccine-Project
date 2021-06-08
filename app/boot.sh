e4fe0eaf283e716ac26d4b850658b4d154a228e0
Matthew Ko <matthewko2020@u.northwestern.edu>
2021-06-08 17:59:05 -0500

    remove boot.sh functionality

--- app/boot.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-
-#python3 run.py create_db
-#python3 run.py acquire #--s3_raw s3://2021-msia423-ko-matthew/raw/pulse2021.csv
-python3 run.py clean #--s3_raw s3://2021-msia423-ko-matthew/raw/pulse2021.csv --s3_clean s3://2021-msia423-ko-matthew/clean/clean.csv
-python3 run.py train #--s3_clean s3://2021-msia423-ko-matthew/clean/clean.csv --s3_enc s3://2021-msia423-ko-matthew/model/encoder.pkl --s3_model s3://2021-msia423-ko-matthew/model/model.pkl --s3_results s3://2021-msia423-ko-matthew/model/results.yaml
-python3 app.py
